1001de258a6c62313e1ab9a7a84b1db9c3ec9033
labile <labile.zhu@gmail.com>
2025-12-24 11:10:44 +0800

    cv

--- bin/local-build.sh
+++ bin/local-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# see `install.sh` first
+
+cd /home/labile/envoy-insider
+
 source .venv/bin/activate
 
 export http_proxy=http://192.168.16.58:8118 && export https_proxy=http://192.168.16.58:8118
